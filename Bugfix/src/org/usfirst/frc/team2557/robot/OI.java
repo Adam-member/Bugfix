2d243c13bb661108385a79d9bb7910077997bc99
Adam Sorg <adamsorg@icloud.com>
2016-01-15 11:39:56 -0800

    bugfix

---
+++ Bugfix/src/org/usfirst/frc/team2557/robot/OI.java
@@ -0,0 +1,66 @@
+package org.usfirst.frc.team2557.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+
+import org.usfirst.frc.team2557.robot.commands.ExampleCommand;
+
+/**
+ * This class is the glue that binds the controls on the physical operator
+ * interface to the commands and command groups that allow control of the robot.
+ */
+public class OI {
+	public Joystick joystick1;
+	public JoystickButton gp1a;
+	public JoystickButton gp1b;
+	public JoystickButton gp1c;
+	public JoystickButton gp1d;
+	public JoystickButton gp1e;
+	public JoystickButton gp1f;
+	public JoystickButton gp1g;
+	
+	joystick1 = new Joystick(0);
+	
+	gp1a.whenPressed(new lift());
+	gp1b.whenPressed(new launch());
+	gp1c.whenPressed(new set());
+	gp1d.whenPressed(new turn());
+	gp1e.whenPressed(new autonomous());
+	gp1f.whenPressed(new lock());
+	
+	
+	
+
+	
+	
+	
+	
+    //// CREATING BUTTONS
+    // One type of button is a joystick button which is any button on a joystick.
+    // You create one by telling it which joystick it's on and which button
+    // number it is.
+    // Joystick stick = new Joystick(port);
+    // Button button = new JoystickButton(stick, buttonNumber);
+    
+    // There are a few additional built in buttons you can use. Additionally,
+    // by subclassing Button you can create custom triggers and bind those to
+    // commands the same as any other Button.
+    
+    //// TRIGGERING COMMANDS WITH BUTTONS
+    // Once you have a button, it's trivial to bind it to a button in one of
+    // three ways:
+    
+    // Start the command when the button is pressed and let it run the command
+    // until it is finished as determined by it's isFinished method.
+    // button.whenPressed(new ExampleCommand());
+    
+    // Run the command while the button is being held down and interrupt it once
+    // the button is released.
+    // button.whileHeld(new ExampleCommand());
+    
+    // Start the command when the button is released  and let it run the command
+    // until it is finished as determined by it's isFinished method.
+    // button.whenReleased(new ExampleCommand());
+}
+
---
+++ Bugfix/src/org/usfirst/frc/team2557/robot/RobotMap.java
@@ -0,0 +1,30 @@
+package org.usfirst.frc.team2557.robot;
+
+import edu.wpi.first.wpilibj.CANTalon;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.RobotDrive;
+
+/**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
+ */
+public class RobotMap {
+	public static DoubleSolenoid launch;
+	public static CANTalon lift;
+	public static CANTalon frontLeft;
+	public static CANTalon frontRight;
+	public static CANTalon backLeft;
+	public static CANTalon backRight;
+	public static RobotDrive robotDrive;
+    // For example to map the left and right motors, you could define the
+    // following variables to use with your drivetrain subsystem.
+    // public static int leftMotor = 1;
+    // public static int rightMotor = 2;
+    
+    // If you are using multiple modules, make sure to define both the port
+    // number and the module. For example you with a rangefinder:
+    // public static int rangefinderPort = 1;
+    // public static int rangefinderModule = 1;
+}
